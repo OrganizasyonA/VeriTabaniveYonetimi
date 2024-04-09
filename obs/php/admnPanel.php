af9c9d245593b411ab4a9e0eaa9a09042f4f7940
Bahattin <72936563+BahattinCinar@users.noreply.github.com>
2024-04-09 23:23:02 +0300

    Add files via upload

--- obs/php/admnPanel.php
+++ obs/php/admnPanel.php
@@ -5,7 +5,7 @@
 <title>Obs</title>
 
 	<!-- Bootstrap -->
-    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+    <link href="../css/bootstrap-4.4.1.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" rel="stylesheet" type="text/css">
 	
@@ -15,21 +15,21 @@
 	
 	<nav class="navbar navbar-dark bg-dark">
 		<a class="navbar-brand" href="userProfile.html">
-			<img class="img" alt="" src="images/profilePic.png" width="30" height="30"/>
-			Kullanici
+			<img class="img" alt="" src="../images/profilePic.png" width="30" height="30"/>
+			<label>Kullanici</label>
 		</a>
 		
 		<div class="navbar-brand text-center">
-			<img class="img" src="images/logo.png" alt="logo" width="30" heigth="30"/>
+			<img class="img" src="../images/logo.png" alt="logo" width="30" heigth="30"/>
 		</div>
 		
 		<div>
-			<a class="navbar-brand float-right" href="loginPage.html">
+			<a class="navbar-brand float-right" href="loginPage.php">
 				
-				<img src="images/logout.png" alt="" height="30" width="30"/>
+				<img src="../images/logout.png" alt="" height="30" width="30"/>
 			</a>
-			<a class="navbar-brand float-right nav-link disabled" href="staffPanel.html">
-				<img src="images/home.png" alt="" height="30" width="30">
+			<a class="navbar-brand float-right nav-link disabled" href="../staffPanel.html">
+				<img src="../images/home.png" alt="" height="30" width="30">
 			</a>
 		</div>
 	</nav>
@@ -37,23 +37,23 @@
 	<div class="container pt-5 text-center text-decoration-none" style="max-width: 90%">
 		<div class="row">
 			<div class="col-sm rounded ml-3 p-2" style="background-color: lightgreen; height: 50px;">
-				<a href="stdntRegister.html" class="text-decoration-none text-primary">
-					Ogrenci Kayit
+				<a href="../stdntRegister.html" class="text-decoration-none text-primary">
+					<label>Ogrenci Kayit</label>
 				</a>
 			</div>
 			<div class="col-sm rounded ml-3 p-2" style="background-color: lightcoral; height: 50px;">
-				<a href="php/pages/not.php" class="text-decoration-none text-primary">
-					Not Bilgisi Girisi
+				<a href="pages/grd.php" class="text-decoration-none text-primary">
+					<label>Not Bilgisi Girisi</label>
 				</a>
 			</div>
 			<div class="col-sm rounded ml-3 p-2" style="background-color: lightblue; height: 50px;">
-				<a href="exmntn.html" class="text-decoration-none text-primary">
-					Yoklama Girisi
+				<a href="../admnExmntn.html" class="text-decoration-none text-primary">
+					<label>Yoklama Girisi</label>
 				</a>
 			</div>
 			<div class="col-sm rounded ml-3 p-2" style="background-color: lightgray; height: 50px;">
-				<a href="php/andmStdntData.php" class="text-decoration-none text-primary">
-					Ogrenci Bilgileri
+				<a href="andmStdntData.php" class="text-decoration-none text-primary">
+					<label>Ogrenci Bilgileri</label>
 				</a>
 			</div>
 		</div>
