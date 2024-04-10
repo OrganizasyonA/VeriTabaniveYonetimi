0766b744e54874a2d6aacd73fe7183755e119496
Bahattin <72936563+BahattinCinar@users.noreply.github.com>
2024-04-10 03:54:09 +0300

    Add files via upload
    mudur/ogretmen/rol eklendi

--- obs/db/ogrenciobs.sql
+++ obs/db/ogrenciobs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 10 Nis 2024, 01:47:53
+-- Üretim Zamanı: 10 Nis 2024, 02:53:02
 -- Sunucu sürümü: 10.4.28-MariaDB
 -- PHP Sürümü: 8.2.4
 
@@ -40,7 +40,8 @@ CREATE TABLE `bolumler` (
 
 INSERT INTO `bolumler` (`BolumID`, `BolumAdi`, `BolumKodu`, `BolumAciklamasi`) VALUES
 (1, 'Bilgisayar', '1', 'Bilgisayar programcilari'),
-(2, 'Makina', '2', 'Makina');
+(2, 'Makina', '2', 'Makina'),
+(3, 'Tum Bolumler', '0', NULL);
 
 -- --------------------------------------------------------
 
@@ -66,11 +67,34 @@ INSERT INTO `dersler` (`DersID`, `DersAdi`, `BolumID`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `mudur`
+--
+
+CREATE TABLE `mudur` (
+  `tcNo` bigint(11) NOT NULL,
+  `kulId` tinyint(1) NOT NULL,
+  `bolumId` tinyint(1) NOT NULL,
+  `ad` char(50) NOT NULL,
+  `soyad` char(50) NOT NULL,
+  `eposta` char(50) NOT NULL,
+  `telNo` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `mudur`
+--
+
+INSERT INTO `mudur` (`tcNo`, `kulId`, `bolumId`, `ad`, `soyad`, `eposta`, `telNo`) VALUES
+(10102020303, 1, 3, 'Kobay', 'Cihat', 'mudur@mudur.com', 2023034444);
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `ogrenci`
 --
 
 CREATE TABLE `ogrenci` (
-  `OgrenciID` int(11) NOT NULL,
+  `kulId` int(11) NOT NULL,
   `Isim` varchar(50) DEFAULT NULL,
   `Soyisim` varchar(50) DEFAULT NULL,
   `tcNum` bigint(11) NOT NULL,
@@ -86,7 +110,7 @@ CREATE TABLE `ogrenci` (
 -- Tablo döküm verisi `ogrenci`
 --
 
-INSERT INTO `ogrenci` (`OgrenciID`, `Isim`, `Soyisim`, `tcNum`, `DogumTarihi`, `Cinsiyet`, `BolumID`, `AnneAdi`, `BabaAdi`, `Adres`) VALUES
+INSERT INTO `ogrenci` (`kulId`, `Isim`, `Soyisim`, `tcNum`, `DogumTarihi`, `Cinsiyet`, `BolumID`, `AnneAdi`, `BabaAdi`, `Adres`) VALUES
 (4, 'enes', 'demir', 20032002198, '1999-02-26', 'Erkek', 1, 'anne', 'baba', 'kocaeli'),
 (5, 'bahattin', 'cinar', 88889999112, '2002-03-22', 'Erkek', 2, 'A', 'B', 'Adres'),
 (6, 'A', 'B', 12345678911, '1999-02-22', 'Kadin', 1, 'B', 'A', 'DADRES');
@@ -113,6 +137,50 @@ INSERT INTO `ogrenci_ders_notlari` (`OgrenciID`, `DersID`, `Vize`, `Final`, `Ode
 (4, 1, 90, 0, 0),
 (4, 2, 0, 80, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ogretmen`
+--
+
+CREATE TABLE `ogretmen` (
+  `tcNo` bigint(11) NOT NULL,
+  `ad` char(50) NOT NULL,
+  `soyad` char(50) NOT NULL,
+  `telNo` bigint(11) NOT NULL,
+  `eposta` char(50) NOT NULL,
+  `bolumId` tinyint(1) NOT NULL,
+  `kulId` tinyint(1) NOT NULL DEFAULT 2
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `ogretmen`
+--
+
+INSERT INTO `ogretmen` (`tcNo`, `ad`, `soyad`, `telNo`, `eposta`, `bolumId`, `kulId`) VALUES
+(20203030445, 'Ali', 'Camdibi', 8885514141, 'ogretmen@ogretmen.com', 1, 2),
+(33445560112, 'Selin', 'Tekgelin', 9094112055, 'ogretmen@ogretmen.com', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `userId`
+--
+
+CREATE TABLE `userId` (
+  `kulId` tinyint(1) NOT NULL,
+  `role` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `userId`
+--
+
+INSERT INTO `userId` (`kulId`, `role`) VALUES
+(1, 3),
+(2, 2),
+(3, 1);
+
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
@@ -131,11 +199,20 @@ ALTER TABLE `dersler`
   ADD KEY `BolumID` (`BolumID`);
 
 --
+-- Tablo için indeksler `mudur`
+--
+ALTER TABLE `mudur`
+  ADD PRIMARY KEY (`tcNo`),
+  ADD KEY `kulId` (`kulId`),
+  ADD KEY `bolumId` (`bolumId`);
+
+--
 -- Tablo için indeksler `ogrenci`
 --
 ALTER TABLE `ogrenci`
-  ADD PRIMARY KEY (`OgrenciID`),
-  ADD KEY `BolumID` (`BolumID`);
+  ADD PRIMARY KEY (`tcNum`),
+  ADD KEY `BolumID` (`BolumID`),
+  ADD KEY `kulId` (`kulId`);
 
 --
 -- Tablo için indeksler `ogrenci_ders_notlari`
@@ -145,6 +222,19 @@ ALTER TABLE `ogrenci_ders_notlari`
   ADD KEY `DersID` (`DersID`);
 
 --
+-- Tablo için indeksler `ogretmen`
+--
+ALTER TABLE `ogretmen`
+  ADD PRIMARY KEY (`tcNo`),
+  ADD KEY `bolumId` (`bolumId`,`kulId`);
+
+--
+-- Tablo için indeksler `userId`
+--
+ALTER TABLE `userId`
+  ADD PRIMARY KEY (`kulId`);
+
+--
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
 
@@ -152,19 +242,13 @@ ALTER TABLE `ogrenci_ders_notlari`
 -- Tablo için AUTO_INCREMENT değeri `bolumler`
 --
 ALTER TABLE `bolumler`
-  MODIFY `BolumID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `BolumID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `dersler`
 --
 ALTER TABLE `dersler`
   MODIFY `DersID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Tablo için AUTO_INCREMENT değeri `ogrenci`
---
-ALTER TABLE `ogrenci`
-  MODIFY `OgrenciID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
